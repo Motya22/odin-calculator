cad5be5516881d4fe79925a6cd1c674d6f272aa8
Motya22 <motya2222@gmail.com>
2023-06-21 11:17:40 +0300

    Create operate function

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+
+    default:
+      break;
+  }
+}
+
 let firstNumber;
 let secondNumber;
 let operator;
