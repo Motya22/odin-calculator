f31a84329f3ba83c57649209414d044ea11be0c7
Motya22 <motya2222@gmail.com>
2023-08-21 17:50:13 +0300

    Create functions that fill the display when the number buttons are pressed.
    Create Create a function that updates the display and a function that fills in the display value.

--- script.js
+++ script.js
@@ -29,3 +29,41 @@ function operate(operator, firstNumber, secondNumber) {
       break;
   }
 }
+
+function updateDisplay() {
+  display.textContent = displayValue;
+}
+
+function numberClick(numberValue) {
+  if (displayValue === '0' || displayValue === firstNumber) {
+    displayValue = numberValue;
+  } else {
+    displayValue += numberValue;
+  }
+
+  updateDisplay();
+}
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let displayValue = '0';
+const display = document.querySelector('.calculator__display');
+const keyboard = document.querySelector('.calculator__keyboard');
+
+keyboard.addEventListener('click', function (e) {
+  const target = e.target;
+
+  if (target.hasAttribute('data-type')) {
+    const buttonType = target.getAttribute('data-type');
+
+    switch (buttonType) {
+      case 'number':
+        numberClick(target.value);
+        break;
+
+      default:
+        break;
+    }
+  }
+});
